487773651f20eb7792a65c998ac236b297a16af6
omri <omrilew93@gmail.com>
2018-11-20 23:04:59 +0200

    Revert "Add files via upload"
    This reverts commit 38b6ef2117c63a2f6a49b7bfdd28df00c30bf5a2.

--- Stations.cpp
+++
@@ -1,5 +0,0 @@
-#include "Stations.h"
-
-int Intercity::derivedSwapTime = 0;
-int Central::derivedSwapTime = 0;
-int Stad::derivedSwapTime = 0;
--- Vehicles.cpp
+++
@@ -1,6 +0,0 @@
-#include "Vehicles.h"
-
-int Rail::derivedStopTime = 0;
-int Tram::derivedStopTime = 0;
-int Bus::derivedStopTime = 0;
-int Sprinter::derivedStopTime = 0;
